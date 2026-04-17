77463e197fa1028aebe40f331ad9b4af019cf3f6
Samson Limanikuki <s11202128@student.usp.ac.fj>
2026-04-18 02:52:54 +1200

    add

--- server/server.js
+++ server/server.js
@@ -7,6 +7,6 @@ app.use(express.json());
 app.use('/', authRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
